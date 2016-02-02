cf54d427144983ed5bf18e4391309146e5ca0f59
Holyoke <kid.idioteque@gmail.com>
2016-02-01 21:37:32 -0800

    Implement insert right

--- linked_list/ruby/lib/linked_list.rb
+++ linked_list/ruby/lib/linked_list.rb
@@ -3,17 +3,26 @@ require "byebug"
 # test = File.read("spec/test_input.txt").split("\n")
 
 class Link
+
+  attr_accessor :next, :prev, :value
+
   def initialize(value = nil)
     @value = value
     @next, @prev = nil, nil
   end
 
   def to_s
-    "%{@value} -> ${@next}"
+    left = @value
+    right = @next.nil? ? "nil" : @next.value
+    
+    "#{left} -> #{right}"
   end
 
   def insert_right(link)
     @next = link
+    link.prev = self
+
     @next
   end
+
 end
--- linked_list/ruby/spec/linked_list_spec.rb
+++ linked_list/ruby/spec/linked_list_spec.rb
@@ -4,18 +4,60 @@ require "byebug"
 
 describe "Linked_List" do
 
-	describe "Link" do
+	describe "Link#" do
 		subject(:link_1) { Link.new("a")}
 		let(:link_2) { Link.new("b")}
+		let(:link_3) { Link.new("c")}
+
+		it "prints the value" do
+			expect(link_1.to_s).to eq("a -> nil")
+		end
+
 		context "inserting a link to the right" do
-
-			it "shows link2 next" do
+			before(:each) do
 				link_1.insert_right(link_2)
+				link_2.insert_right(link_3)
+			end
 
+			it "points next as link2" do
 				expect(link_1.next).to eq(link_2)
 			end
 
-			it "shows nil previously"
+			it "points nil previously" do
+				expect(link_1.prev).to eq(nil)
+			end
+
+			it "points link_2's next as link_3" do
+				expect(link_2.next).to eq(link_3)
+			end
+
+			it "points link_2's prev as link_1" do
+				expect(link_2.prev).to eq(link_1)
+			end
+
+			it "points link_3's next as nil" do
+				expect(link_3.next).to eq(nil)
+			end
+
+			it "points link_3's prev as link_2" do
+				expect(link_3.prev).to eq(link_2)
+			end
+
+			context "printing pointers" do
+				it "prints link_1" do
+					expect(link_1.to_s).to eq("a -> b")
+				end
+
+				it "prints link_2" do
+					expect(link_2.to_s).to eq("b -> c")
+				end
+
+				it "prints link_3" do
+					expect(link_3.to_s).to eq("c -> nil")
+				end
+			end
+
 		end
+
 	end
 end
